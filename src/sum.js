a9ce4b73f1f5593942545fb4dcb5b4e7c489d066
sxs <sxs@xps.com>
2023-04-24 16:12:40 +0200

    Add vitest
    test suite

---
+++ src/sum.js
@@ -0,0 +1,9 @@
+/**
+ * sum 2 values
+ * @param  {number} a
+ * @param  {number} b
+ * @return {number}
+ */
+export function sum(a, b) {
+  return a + b;
+}
---
+++ src/sum.test.js
@@ -0,0 +1,20 @@
+import { assert, describe, expect, it } from "vitest";
+import { sum } from "./sum";
+
+describe("suite name", () => {
+  it("foo", () => {
+    assert.equal(Math.sqrt(4), 2);
+  });
+
+  it("bar", () => {
+    expect(1 + 1).eq(2);
+  });
+
+  it("snapshot", () => {
+    expect({ foo: "bar" }).toMatchSnapshot();
+  });
+
+  it("sum", () => {
+    expect(sum(1, 1)).equal(2);
+  });
+});
