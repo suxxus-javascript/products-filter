dd17730a72f2204f19e5d323755b72f4e749b7be
sxs <sxs@xps.com>
2023-04-24 18:12:02 +0200

    Add tailwindcss

--- src/client/main.js
+++ src/client/main.js
@@ -1,7 +1,5 @@
-import "./style.css";
-
 document.querySelector("#app").innerHTML = `
-  <div>
-   <p>Hello</p>
-  </div>
+    <div class="bg-blue-500 text-white p-4">
+      <h1 class="text-2xl">Welcome to my project!</h1>
+    </div>
 `;
